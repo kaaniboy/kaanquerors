5ceb9def1a14d481cf5397ff1d9d542c0158355b
Kaan Aksoy <Kaan.Aksoy@aexp.com>
2017-07-13 11:03:49 -0700

    Create basic version of Github API

--- server.py
+++ server.py
@@ -1,6 +1,41 @@
 from flask import Flask
+from flask import jsonify
+from flask import request
+from github import Github
+from auth import getProxyURL, getUsername, getPassword
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello World!"
+    return 'Github Recruiter'
+
+@app.route('/get_profiles')
+def get_profiles():
+    school = request.args.get('school', '')
+    
+    os.environ['HTTP_PROXY'] = getProxyURL()
+
+    g = Github(getUsername(), getPassword())
+
+    results = []
+
+    for user in g.search_users(school).get_page(0):
+        item = {
+            'name': user.name,
+            'email': user.email,
+            'avatar_url': user.avatar_url,
+            'url': user.url,
+            
+        }
+        
+        results.append(item)
+        
+    return jsonify({'users': results})
+    
+
+@app.route('/proxy')
+def get_proxy():
+    return getProxyURL()
+    
